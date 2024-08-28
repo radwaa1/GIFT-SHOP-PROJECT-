48e8ff44ab2074b178e472e20c293e54847a9acf
Radwa Mohamed <118927521+radwaa1@users.noreply.github.com>
2024-08-28 22:57:52 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+// document.getElementById('').innerHTML = 'Hello JavaScript';
+// .onclick = function(){myScript};
+
+// Cart
+
+// let cartIcon = document.querySelector("#cart-icon");
+//  let cart = document.querySelector(".cart"); 
+//  let closeCart = document.querySelector("#close-cart");
+
+// cartIcon.onclick = () => {
+
+// cart.classList.add("active");
+
+// };
